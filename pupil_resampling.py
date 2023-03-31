2aedc2d68b34b3f04f49323ab8d7c1ddb54c994e
marcoghezzi1 <marco.ghezzi3@studenti.unimi.it>
2023-03-31 10:02:50 +0200

    Update pupil_resampling.py (-S to run it on Pascal)

--- pupil_resampling.py
+++ pupil_resampling.py
@@ -1,9 +1,26 @@
 import pandas as pd
 import os
 from scipy import signal
+import argparse
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("-S", "--server", action="store_true", help='path from server')
+
+args = parser.parse_args()
+config = vars(args)
+
+check = config['server']
 
 # tmp absolute path
-dir_pupil = '/Users/marcoghezzi/PycharmProjects/pythonProject/fear_gen/data/tmp_data/tmp_pupil'
+if check:
+    dir_pupil = '/var/data/FEAR_GEN/tmp_data/tmp_pupil'
+    data_path = '/var/data/FEAR_GEN/'
+else:
+    print("Enter your local directory for pupil csv: ")
+    dir_pupil = input()
+    data_path = '.'
+
 
 for file in sorted(os.listdir(dir_pupil)):
 
@@ -16,5 +33,6 @@ for file in sorted(os.listdir(dir_pupil)):
         resampled_values = signal.resample(values, 1000)
         resampled.append(resampled_values)
     df_resampled = pd.DataFrame(resampled)
-    # df_resampled.to_csv('output path del server')
+    if check:
+        df_resampled.to_csv(data_path+'output/resampled/resampled_'+file)
 
