c81ff337b915c6bfaea220540ddc56e7e2647b75
marcoghezzi1 <marco.ghezzi3@studenti.unimi.it>
2023-04-28 10:24:01 +0200

    parallel rf

--- kfold_subject_social_anxiety.py
+++ kfold_subject_social_anxiety.py
@@ -347,9 +347,9 @@ def get_features(data, config='all_features', typ='sac'):
 # MAIN ---------------------------------------------------------------------
 
 dataset_name = 'Reutter_OU_posterior_VI'
-models_regression = [ #'GPR',
-                      #SVR( C=1000, kernel='rbf', gamma=0.002), 
-                      RandomForestRegressor(n_estimators=1), 
+models_regression = [ 'GPR',
+                      SVR( C=1000, kernel='rbf', gamma=0.002),
+                      RandomForestRegressor(n_estimators=1,n_jobs=32),
                       MLPRegressor(hidden_layer_sizes=(100, 50, 25))
                     ]
 
